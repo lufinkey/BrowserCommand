026265cea6b19edbc0c2f166c73cb31dadbe0ee4
lufinkey <luisfinke@gmail.com>
2017-12-20 22:37:17 -0500

    declare the web controller

--- crx-controller.js
+++ crx-controller.js
@@ -7,10 +7,13 @@ webSocket = new WebSocket(SOCKET_URL);
 
 webSocket.onopen = function(evt) {
 	console.log("websocket connected to "+SOCKET_URL);
+	webSocket.send(JSON.stringify({
+		type:'req-declare-controller'
+	}));
 }
 
 webSocket.onclose = function(evt) {
-	console.log("websocket closed");
+	console.log("websocket closed", evt);
 }
 
 webSocket.onerror = function(evt) {
@@ -19,4 +22,14 @@ webSocket.onerror = function(evt) {
 
 webSocket.onmessage = function(evt) {
 	console.log("webSocket message", evt);
+	var message = JSON.parse(evt.data);
+	if(message.type=='resp-declare-controller')
+	{
+		console.log(message.message);
+		if(!message.success)
+		{
+			console.log("closing websocket");
+			webSocket.close(1, "couldn't declare controller");
+		}
+	}
 }
--- crx-server.js
+++ crx-server.js
@@ -6,34 +6,54 @@ var server = new http.Server();
 var wsServer = new http.WebSocketServer(server);
 server.listen(config.PORT, config.HOST);
 
-console.log("server listening on port "+PORT);
+console.log("server listening on port "+config.PORT);
 
 server.addEventListener('request', function(req) {
+	console.log("received http request", req);
 	return false;
 });
 
-var connectedSockets = [];
+var controllerSocket = null;
 
 wsServer.addEventListener('request', function(req) {
 	console.log("received request", req);
 
 	var socket = req.accept();
-	connectedSockets.push(socket);
 
 	socket.addEventListener('message', function(evt) {
 		console.log("received message", evt);
+		var message = JSON.parse(evt.data);
+		if(message.type=='req-declare-controller')
+		{
+			if(controllerSocket!=null)
+			{
+				socket.send(JSON.stringify({
+					type:'resp-declare-controller',
+					success:false,
+					message:"controller socket already declared"
+				}));
+				return;
+			}
+			controllerSocket = socket;
+			socket.send(JSON.stringify({
+				type:'resp-declare-controller',
+				success:true,
+				message:"controller declared"
+			}));
+		}
 	});
 
 	socket.addEventListener('close', function() {
 		console.log("disconnected socket", socket);
-		for(var i=0; i<connectedSockets.length; i++)
+		if(socket==controllerSocket)
 		{
-			if(connectedSockets[i]==socket)
-			{
-				connectedSockets.splice(i, 1);
-				break;
-			}
+			controllerSocket = null;
+			console.log("lost web controller socket");
 		}
+	});
+
+	socket.addEventListener('error', function(evt) {
+		console.log("received error", evt);
 	});
 
 	return true;
