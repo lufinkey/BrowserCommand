a6c7dfa4c4294b9c8d4585497b343ba5f806d18a
lufinkey <luisfinke@gmail.com>
2017-12-26 18:05:29 -0500

    correctly parse windowId

--- cli.js
+++ cli.js
@@ -2,30 +2,65 @@
 const ChromeBridge = require('./lib/ChromeBridge');
 
 
+//functions
+function print_window(window)
+{
+	var props = [ "id", "type", "state", "focused", "incognito", "top", "left", "width", "height" ];
+	for(var i=0; i<props.length; i++)
+	{
+		var key = props[i];
+		var value = window[key];
+		console.log(key+': '+value);
+	}
+}
+
+function isIntegerString(str)
+{
+	var numbers = [ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' ];
+	for(var i=0; i<str.length; i++)
+	{
+		if(numbers.indexOf(str.charAt(i)) < 0)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+
 //parse arguments
 var request = null;
-var callback = (response, error) => {
-	if(error)
-	{
-		console.error(error);
-	}
-	else
-	{
-		console.log(response);
-	}
+var callback = (response) => {
+	console.log(response);
 };
 switch(process.argv[2])
 {
 	case 'window':
 		switch(process.argv[3])
 		{
-			case undefined:
 			case 'list':
 				request = {type: 'get-windows'};
+				callback = (windows) => {
+					for(var i=0; i<windows.length; i++)
+					{
+						print_window(windows[i]);
+						if(i != (windows.length-1))
+						{
+							console.log('');
+						}
+					}
+				};
 				break;
 
 			case 'get':
+				if(isIntegerString(process.argv[4]))
+				{
+					process.argv[4] = Number.parseInt(process.argv[4]);
+				}
 				request = {type: 'get-window', windowId: process.argv[4]};
+				callback = (window) => {
+					print_window(window);
+				};
 				break;
 
 			default:
@@ -73,7 +108,7 @@ bridge.on('connect', () => {
 		}
 		else
 		{
-			console.log(response);
+			callback(response);
 			process.exit(0);
 		}
 	});
