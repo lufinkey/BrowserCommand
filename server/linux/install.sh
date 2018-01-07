73850cfa9e7e17f0b5a2b5368e8e5e2bcc8008f5
lufinkey <luisfinke@gmail.com>
2018-01-07 00:29:26 -0500

    linux service

---
+++ server/linux/install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Ensure root
+if [[ $EUID -ne 0 ]]
+then
+	>&2 echo "You must be root to install the service"
+	exit 1
+fi
+
+# Moving into script directory
+base_dir=$(dirname "${BASH_SOURCE[0]}")
+cd "$base_dir"
+
+# Create chrome-cmd user if needed
+if [ -z "$(id -u chrome-cmd)" &> /dev/null ]
+then
+	echo "creating user chrome-cmd"
+	useradd --system --no-create-home --shell "/bin/false" chrome-cmd
+	nobody_home=~nobody_home
+	if [ -n "$nobody_home" ]
+	then
+		usermod -d "$nobody_home" chrome-cmd
+	fi
+fi
+
+cp "chrome-cmd.service" "/etc/init.d/chrome-cmd"
+
+update-rc.d chrome-cmd defaults
