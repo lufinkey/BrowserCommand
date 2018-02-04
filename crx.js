c4d9f86a26f70752b1f976b7a7e135f6893fa18f
lufinkey <luisfinke@gmail.com>
2018-02-04 01:12:24 -0500

    load saved preferences when controller loads

--- crx.js
+++ crx.js
@@ -2,16 +2,21 @@
 const ChromeBridgeController = require('./lib/ChromeBridgeController');
 
 const backgroundPage = chrome.extension.getBackgroundPage();
-
-var controllerOptions = {
-	verbose: true,
-	outputFunctionsInJSON: true
-};
-var controller = new ChromeBridgeController(controllerOptions);
-controller.start();
-
-controller.on('retryConnect', () => {
-	//console.clear();
-});
-
+var controller = new ChromeBridgeController();
 backgroundPage.controller = controller;
+
+chrome.storage.local.get(['port', 'identifier'], (items) => {
+	var options = {
+		verbose: true,
+		outputFunctionsInJSON: true,
+		port: items.port,
+		identifier: items.identifier
+	};
+	controller.setOptions(options);
+
+	controller.on('retryConnect', () => {
+		//console.clear();
+	});
+
+	controller.start();
+});
