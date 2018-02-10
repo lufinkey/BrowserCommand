75bebbd932d668dd55eacd00f73a2e1dc79d6445
lufinkey <luisfinke@gmail.com>
2018-02-09 21:30:15 -0500

    export more shit

--- index.js
+++ index.js
@@ -2,9 +2,13 @@
 const BrowserBridgeClient = require('./lib/BrowserBridgeClient');
 const BrowserBridgeServer = require('./lib/BrowserBridgeServer');
 const BrowserBridgeController = require('./lib/BrowserBridgeController');
+const JobManager = require('./lib/JobManager');
+const UserKeyManager = require('./lib/UserKeyManager');
 
 module.exports = {
 	Client: BrowserBridgeClient,
 	Server: BrowserBridgeServer,
-	Controller: BrowserBridgeController
+	Controller: BrowserBridgeController,
+	JobManager: JobManager,
+	UserKeyManager: UserKeyManager
 };
