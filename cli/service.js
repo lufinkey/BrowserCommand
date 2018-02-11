6c456f64958657e674b2e3b61faefcd834809469
lufinkey <luisfinke@gmail.com>
2018-02-11 15:08:12 -0500

    cleaner service management for linux

--- cli/service.js
+++ cli/service.js
@@ -1,124 +1,51 @@
 
 const ArgParser = require('../lib/ArgParser');
-const ChildProcess = require('child_process');
+const { spawnSync } = require('child_process');
 const elevationinfo = require('elevationinfo');
+const fs = require('fs');
 const os = require('os');
 
 
 
 module.exports = function(cli, callback, ...args)
 {
-	var serviceCommand = args[0];
+	var platform = os.platform();
+	var command = args[0];
 	args = args.slice(1);
-	switch(serviceCommand)
+	switch(command)
 	{
 		case 'install':
-			// parse args
-			var serviceOptions = {
-				args: [
-					{
-						name: 'ignore-if-nonroot',
-						type: 'boolean',
-						default: false
-					}
-				],
-				stopAtError: true,
-				errorExitCode: 1
-			};
-			var serviceArgv = ArgParser.parse(args, serviceOptions);
-			switch(os.platform())
-			{
-				case 'linux':
-					// ensure root
-					if(!elevationinfo.isElevated())
-					{
-						if(serviceArgv.args['ignore-if-nonroot'])
-						{
-							callback(0);
-							return;
-						}
-						console.error("root permissions are required to run this command");
-						callback(1);
-						return;
-					}
-					// run install script
-					var installerProcess = ChildProcess.spawn(cli.basedir+'/service/linux/install.sh', [], { cwd: __dirname, stdio: 'inherit' });
-					installerProcess.on('exit', (code, signal) => {
-						if(code != 0)
-						{
-							console.error("errors occurred while installing service");
-							callback(code);
-							return;
-						}
-						callback(0);
-					});
-					break;
-
-				default:
-					console.error("command not supported by this platform");
-					callback(1);
-					break;
-			}
-			break;
-
 		case 'uninstall':
-			// parse args
-			var serviceOptions = {
-				args: [
-					{
-						name: 'ignore-if-nonroot',
-						type: 'boolean',
-						default: false
-					}
-				],
-				stopAtError: true,
-				errorExitCode: 1
-			};
-			var serviceArgv = ArgParser.parse(args, serviceOptions);
-			// check platform
-			switch(os.platform())
+		case 'enable':
+		case 'disable':
+		case 'start':
+		case 'stop':
+		case 'restart':
+			// ensure the script exists
+			if(!fs.existsSync(cli.basedir+'/service/'+platform+'/manage'))
 			{
-				case 'linux':
-					// ensure root
-					if(!elevationinfo.isElevated())
-					{
-						if(serviceArgv.args['ignore-if-nonroot'])
-						{
-							callback(0);
-							return;
-						}
-						console.error("root permissions are required to run this command");
-						callback(1);
-						return;
-					}
-					// run uninstall script
-					var installerProcess = ChildProcess.spawn(cli.basedir+'/service/linux/uninstall.sh', [], { cwd: __dirname, stdio: 'inherit' });
-					installerProcess.on('exit', (code, signal) => {
-						if(code != 0)
-						{
-							console.error("errors occurred while installing service");
-							callback(code);
-							return;
-						}
-						callback(0);
-					});
-					break;
-
-				default:
-					console.error("command not supported by this platform");
-					process.exit(1);
-					break;
+				console.error("command is not supported by this platform");
+				callback(1);
+				return;
 			}
+			// run the script
+			var result = spawnSync(cli.basedir+'/service/'+platform+'/manage', [command].concat(args), { cwd: cli.basedir, stdio: 'inherit' });
+			if(result.status != 0)
+			{
+				callback(result.status);
+				return;
+			}
+			callback(0);
 			break;
 
 		case undefined:
 			console.error("no command specified");
-			process.exit(1);
+			callback(1);
 			break;
 
 		default:
-			console.error("invalid command "+serviceCommand);
-			process.exit(1);
+			console.error("invalid command "+command);
+			callback(1);
 			break;
 	}
 }
